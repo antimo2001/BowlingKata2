cc11f40c4fb8c8cf6c0d900dbc3819d682e2e025
USLMAAGERONIMO4 <AGeronimo@Deloitte.com>
2019-01-31 12:13:33 -0500

    more testcases for Utility class

--- test/Utility.test.ts
+++ test/Utility.test.ts
@@ -1,7 +1,8 @@
 import 'mocha';
-// import debug from 'debug';
+import debug from 'debug';
 import { expect } from 'chai';
 import { Utility } from '../src/Utility';
+const debugFip = debug("fip01:test:Utility");
 
 describe("Utility", function() {
     describe("#range", function() {
@@ -24,6 +25,28 @@ describe("Utility", function() {
             const actual = Utility.range(3, 10, 2);
             const expectedRange = [3, 5, 7, 9];
             expect(actual).to.have.ordered.members(expectedRange);
+        });
+    });
+    describe("#stall", function() {
+        it("stalls in ascending order", async function() {
+            let nums: number[] = [1, 2];
+            await Promise.all([
+                Utility.stall(29).then(() => nums.push(9)),
+                Utility.stall(17).then(() => nums.push(7)),
+                Utility.stall(5).then(() => nums.push(5)),
+            ]);
+            debugFip(`nums===${nums}`);
+            expect(nums).ordered.members([1, 2, 5, 7, 9]);
+        });
+        it("stalls in descending order", async function() {
+            let nums: number[] = [100, 99];
+            await Promise.all([
+                Utility.stall(29).then(() => nums.push(69)),
+                Utility.stall(17).then(() => nums.push(77)),
+                Utility.stall(5).then(() => nums.push(85)),
+            ]);
+            debugFip(`nums===${nums}`);
+            expect(nums).ordered.members([100, 99, 85, 77, 69]);
         });
     });
 });
