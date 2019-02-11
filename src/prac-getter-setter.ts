5d8a83f4001afc7aaac3b7824794b1f66d3f3eb1
USLMAAGERONIMO4 <AGeronimo@Deloitte.com>
2019-02-11 16:51:58 -0500

    more practicing with getters

--- src/prac-getter-setter.ts
+++ src/prac-getter-setter.ts
@@ -1,29 +1,47 @@
+import debug from 'debug'
+
+const debugFip = debug('src:prac-getter-setter')
+
 /**
  * Class is for practicing getters and setters of properties.
- * Found a helpful question on stackoverflow: https://stackoverflow.com/questions/12827266/get-and-set-in-typescript
+ *
+ * Found a helpful question on stackoverflow:
+ * https://stackoverflow.com/questions/12827266/get-and-set-in-typescript
+ *
+ * Also, see the official typescript docs: http://www.typescriptlang.org/docs/handbook/classes.html
  */
-export class Greeter {
-    private _name: string;
+class Greeter {
+    private _name: string
 
     constructor() {
-        this._name = "";
+        this._name = "unnamed"
     }
 
     /**
      * Get the name of this greeter or set its value
      */
     public get name() : string {
-        return this._name;
+        return this._name
     }
     public set name(v : string) {
-        this._name = v;
+        this._name = v
     }
 
     /**
      * Example of invoking the getter
      */
     public hello(): string {
-        return `Hello, ${this.name}`;
+        return `Hello, ${this.name}`
     }
 
+}
+
+/**
+ * ----------------------------------- MAIN -----------------------------------
+ */
+{
+    const greeter = new Greeter()
+    greeter.name = 'Alice'
+    // greeter.name = 9 ////causes compile time error
+    debugFip(greeter.hello())
 }
